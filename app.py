e63f84599bbd8eb8e775654a61596c2f8c4a205a
Bomani McClendon <bomanimcclendon2017@u.northwestern.edu>
2016-02-10 13:00:14 -0600

    Sending back a basic 404 when consumer request isn't good, but there needs to be smarter ways to look at this

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def developEmbed():
 	fullPath = request.url
 	urlChunks = fullPath.split("/?url=")
 
-	if len(urlChunks) <= 2:
+	if len(urlChunks) == 2:
 		url = urlChunks[1]
 		result = parseTimeline(url)
 
@@ -30,7 +30,7 @@ def developEmbed():
 
 		return resp
 	else:
-		return jsonify({'result': "This is an erroneous request."})
+		return jsonify({'result': "This is an erroneous request."}), 404
 
 
 #This function needs to consider all of the options for timelines.
@@ -92,34 +92,5 @@ def structureResponse(html, width, height):
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-# type (required) : The resource type. Valid values, along with value-specific 
-# parameters, are described below.
-
-# version (required): The oEmbed version number. This must be 1.0.
-
-# title (optional) : A text title, describing the resource.
-
-# author_name (optional) : The name of the author/owner of the resource.
-
-# author_url (optional) : A URL for the author/owner of the resource.
-
-# provider_name (optional) : The name of the resource provider.
-
-# provider_url (optional) : The url of the resource provider.
-
-# cache_age (optional) : The suggested cache lifetime for this resource, in 
-# seconds. Consumers may choose to use this value or not.
-
-# thumbnail_url (optional) : A URL to a thumbnail image representing the resource. 
-# The thumbnail must respect any maxwidth and maxheight parameters. If this parameter 
-# is present, thumbnail_width and thumbnail_height must also be present.
-
-# thumbnail_width (optional) : The width of the optional thumbnail. If this parameter
-# is present, thumbnail_url and thumbnail_height must also be present.
-
-# thumbnail_height (optional) : The height of the optional thumbnail. If this 
-# parameter is present, thumbnail_url and thumbnail_width must also be present.
 
 
