59c6cbef05b32d8c93e26f057d25c4d22de6c6d8
Bomani McClendon <bomanimcclendon2017@u.northwestern.edu>
2016-02-10 11:00:34 -0600

    Began to add some functionality for structuring JSON oEmbed responses

--- app.py
+++ app.py
@@ -5,9 +5,10 @@ import urllib
 
 app = Flask(__name__)
 
+time
+
 # Format for the oEmbed requests:
 # oembed.knightlab.com?url=<a URL to a timeline>
-# 
 @app.route('/', methods=['GET'])
 def get_url_param():
 	fullPath = request.url
@@ -20,6 +21,8 @@ def get_url_param():
 	else:
 		return jsonify({'result': "This is an erroneous request."})
 
+
+#This function needs to consider all of the options for timelines.
 def parseTimeline(url):
 	timelineURL = urlparse(urllib.unquote(url).decode('utf8'))
 	if("timeline" in timelineURL.path):
@@ -30,8 +33,51 @@ def parseTimeline(url):
 
 	return timelineURL
 
-# def structureJSON(type, ):
-	
+
+def structureJSON(html):
+
+	responseJSON = {}
+
+	#For Knight Lab Tools, we will need a `rich` type.
+	responseJSON['type'] = 'rich'
+
+	#oEmbed explains that the version must be 1.
+	responseJSON['version'] = 1.0
+
+	#For rich-type content, oEmbed requires html, 
+	#width, and height parameters.
+	responseJSON['html'] = html
+
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+# type (required) : The resource type. Valid values, along with value-specific 
+# parameters, are described below.
+
+# version (required): The oEmbed version number. This must be 1.0.
+
+# title (optional) : A text title, describing the resource.
+
+# author_name (optional) : The name of the author/owner of the resource.
+
+# author_url (optional) : A URL for the author/owner of the resource.
+
+# provider_name (optional) : The name of the resource provider.
+
+# provider_url (optional) : The url of the resource provider.
+
+# cache_age (optional) : The suggested cache lifetime for this resource, in 
+# seconds. Consumers may choose to use this value or not.
+
+# thumbnail_url (optional) : A URL to a thumbnail image representing the resource. 
+# The thumbnail must respect any maxwidth and maxheight parameters. If this parameter 
+# is present, thumbnail_width and thumbnail_height must also be present.
+
+# thumbnail_width (optional) : The width of the optional thumbnail. If this parameter
+# is present, thumbnail_url and thumbnail_height must also be present.
+
+# thumbnail_height (optional) : The height of the optional thumbnail. If this 
+# parameter is present, thumbnail_url and thumbnail_width must also be present.
+
