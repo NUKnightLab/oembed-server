f015f703e392ff3a564ad06f620d6a7e1262e7af
Scott Bradley <scottbradley.mail@gmail.com>
2016-06-24 10:12:06 -0500

    moves wsgi to project root for now

--- conf/stg/wsgi.py
+++
@@ -1,9 +0,0 @@
-import os
-import site
-import sys
-
-site.addsitedir('/home/apps/env/oembed-server/lib/python2.7/site-packages')
-sys.path.append('/home/apps/sites/oembed-server')
-sys.stdout = sys.stderr
-
-from app import app as application
